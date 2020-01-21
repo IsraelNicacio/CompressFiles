5d309e13feb1602efe881ed7cdbc7028f4200c4e
Israel <israel.nicacio@outlook.com>
2020-01-21 09:53:06 -0300

    Add compress

---
+++ CompressFiles/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CompressFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstRegistros = new System.Windows.Forms.ListBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.btnSelectPath = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(65, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(440, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(560, 12);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 20);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "* Diretório:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar.ForeColor = System.Drawing.Color.Green;
+            this.progressBar.Location = new System.Drawing.Point(5, 16);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(620, 26);
+            this.progressBar.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.progressBar);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(5, 221);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(5, 3, 5, 5);
+            this.groupBox2.Size = new System.Drawing.Size(630, 47);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Progresso";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstRegistros);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(5, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 3, 5, 4);
+            this.groupBox1.Size = new System.Drawing.Size(630, 180);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registros";
+            // 
+            // lstRegistros
+            // 
+            this.lstRegistros.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstRegistros.FormattingEnabled = true;
+            this.lstRegistros.Location = new System.Drawing.Point(5, 16);
+            this.lstRegistros.Name = "lstRegistros";
+            this.lstRegistros.Size = new System.Drawing.Size(620, 160);
+            this.lstRegistros.TabIndex = 3;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            // 
+            // btnSelectPath
+            // 
+            this.btnSelectPath.Location = new System.Drawing.Point(511, 12);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(25, 20);
+            this.btnSelectPath.TabIndex = 8;
+            this.btnSelectPath.Text = "...";
+            this.btnSelectPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSelectPath.UseVisualStyleBackColor = true;
+            this.btnSelectPath.Click += new System.EventHandler(this.btnSelectPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 273);
+            this.Controls.Add(this.btnSelectPath);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstRegistros;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Button btnSelectPath;
+    }
+}
+
---
+++ CompressFiles/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace CompressFiles
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo directorySelected;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(txtPath.Text))
+            {
+                directorySelected = new DirectoryInfo(txtPath.Text);
+
+                string[] arrayXml = new string[directorySelected.GetFiles().Length];
+                int count = 0;
+
+                foreach (FileInfo fi in directorySelected.GetFiles())
+                {
+                    byte[] fileToBytes = File.ReadAllBytes(fi.FullName);
+
+                    string value = ASCIIEncoding.UTF8.GetString(fileToBytes);
+
+                    arrayXml[count] = value;
+
+                    count++;
+                }
+            }
+
+            //backgroundWorker.RunWorkerAsync();
+        }
+
+        
+
+        private void btnSelectPath_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog frmFolderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (frmFolderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Atualiza caixa de texto
+                    txtPath.Text = frmFolderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            FileInfo[] files = directorySelected.GetFiles();
+
+            //Variaveis
+            int count = 0;
+            int total = files.Length;
+
+            foreach (FileInfo file in files)
+            {
+                // Get the stream of the source file.
+                using (FileStream inFile = file.OpenRead())
+                {
+                    // Prevent compressing hidden and 
+                    // already compressed files.
+                    if ((File.GetAttributes(file.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden & file.Extension != ".gz")
+                    {
+                        // Create the compressed file.
+                        using (FileStream outFile = File.Create(file.FullName + ".gz"))
+                        {
+                            using (GZipStream Compress = new GZipStream(outFile, CompressionMode.Compress))
+                            {
+                                // Copy the source file into 
+                                // the compression stream.
+                                //inFile.CopyTo(Compress);
+
+                                Console.WriteLine("Compressed {0} from {1} to {2} bytes.",
+                                    file.Name, files.Length.ToString(), outFile.Length.ToString());
+                            }
+                        }
+                    }
+                }
+
+                Thread.Sleep(100);
+
+                //Increment count
+                count++;
+
+                //Porcentagem
+                int porcentagem = ((int)count * 100) / total;
+
+                // Report progress.
+                backgroundWorker.ReportProgress(porcentagem);
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Change the value of the ProgressBar to the BackgroundWorker progress.
+            progressBar.Value = e.ProgressPercentage;
+            progressBar.PerformStep();
+            // Set the text.
+            this.Text = e.ProgressPercentage.ToString();
+        }
+
+        public static byte[] Compress(byte[] raw)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (GZipStream gzip = new GZipStream(memory,
+                    CompressionMode.Compress, true))
+                {
+                    gzip.Write(raw, 0, raw.Length);
+                }
+                return memory.ToArray();
+            }
+        }
+    }
+}
